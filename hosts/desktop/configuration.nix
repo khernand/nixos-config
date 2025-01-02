a47d8cfcd86788a953a9ac40c0b1efd12b5131ca
Kevin Hernandez <kphernand@gmail.com>
2025-01-02 15:01:21 -0500

    system packages

--- hosts/desktop/configuration.nix
+++ hosts/desktop/configuration.nix
@@ -42,15 +42,6 @@
     ];
   };
 
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
-  environment.systemPackages = with pkgs; [
-    vim
-    wget
-    cudatoolkit
-    git
-  ];
-
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
