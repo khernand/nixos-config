f3aebcb229459d7c57add2a424927d5590ea92c7
Kevin Hernandez <2459865+khernand@users.noreply.github.com>
2025-05-05 17:11:33 -0400

    nordic

--- modules/home-manager/desktop/hyprland/gtk/default.nix
+++ modules/home-manager/desktop/hyprland/gtk/default.nix
@@ -1,25 +1,33 @@
 {
   userConfig,
   pkgs,
-  catppuccin,
   ...
 }: {
   # GTK theme configuration
   gtk = {
     enable = true;
+
+    theme = {
+      name = "Nordic";
+      package = pkgs.nordic;
+    };
+
     iconTheme = {
       name = "Tela-circle-dark";
       package = pkgs.tela-circle-icon-theme;
     };
+
     cursorTheme = {
       name = "Yaru";
       package = pkgs.yaru-theme;
       size = 24;
     };
+
     font = {
       name = "Roboto";
       size = 11;
     };
+    
     gtk3 = {
       bookmarks = [
         "file:///home/${userConfig.name}/Documents"
