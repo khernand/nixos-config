9f97178cac44b340edbb0aacd939c601c26f5a68
Kevin Hernandez <2459865+khernand@users.noreply.github.com>
2025-06-18 12:59:26 -0400

    Use cattpuccin

--- modules/home-manager/desktop/hyprland/dependencies/clipse.nix
+++ modules/home-manager/desktop/hyprland/dependencies/clipse.nix
@@ -1,5 +1,13 @@
 {...}: {
   services.clipse = {
     enable = true;
+
+    historySize = 200;
+    imageDisplay = {
+       type = "kitty";
+       scaleX = 9;
+       scaleY = 9;
+       heightCut = 2;
+    };
   };
 }
--- modules/home-manager/programs/kitty.nix
+++ modules/home-manager/programs/kitty.nix
@@ -1,9 +1,8 @@
 {
-  dotfiles,
   dotfilesDir,
   ...
 }: {
-  home.file.".config/kitty/KittyTheme.conf".source = "${dotfiles}/KittyTheme.conf";
+  home.file.".config/kitty/KittyTheme.conf".source = "${dotfilesDir}/kitty/KittyTheme.conf";
   home.file.".config/kitty/nvimSplit.conf".source = "${dotfilesDir}/kitty/nvimSplit.conf";
 
   programs.kitty = {
@@ -11,7 +10,7 @@
     extraConfig = ''
       font_family Fira Code
       font_size 12
-      include ${dotfiles}/KittyTheme.conf
+      include ${dotfilesDir}/kitty/KittyTheme.conf
       allow_remote_control yes
       map ctrl+down resize_window shorter
       map ctrl+up resize_window taller
