63f2d37d7f3f31de078cd7d5627df3be80baee5f
Kevin Hernandez <2459865+khernand@users.noreply.github.com>
2025-07-11 16:00:23 -0400

    try flake.nix updates

--- flake.nix
+++ flake.nix
@@ -5,6 +5,9 @@
     # Nixpkgs
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-24.11";
+    chaotic.url = "github:chaotic-cx/nyx/nyxpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    agenix.url = "github:ryantm/agenix";
 
     # Home manager
     home-manager = {
@@ -27,26 +30,51 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    # Homebrew
-    nix-homebrew.url = "github:zhaofengli-wip/nix-homebrew";
-
-    chaotic.url = "github:chaotic-cx/nyx/nyxpkgs-unstable";
+    nix-homebrew = {
+      url = "github:zhaofengli-wip/nix-homebrew";
+    };
+    homebrew-bundle = {
+      url = "github:homebrew/homebrew-bundle";
+      flake = false;
+    };
+    homebrew-core = {
+      url = "github:homebrew/homebrew-core";
+      flake = false;
+    };
+    homebrew-cask = {
+      url = "github:homebrew/homebrew-cask";
+      flake = false;
+    };
+    secrets = {
+      url = "git+ssh://git@github.com/khernand/nix-secrets.git";
+      flake = false;
+    };
+    niri-flake = {
+      url = "github:sodiboo/niri-flake";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = 
   { 
     self,
+    agenix,
     catppuccin,
     darwin,
     home-manager,
     nix-homebrew,
+    homebrew-bundle, 
+    homebrew-core, 
+    homebrew-cask,
+    flake-utils,
     nixpkgs,
+    niri-flake,
+    secrets,
     dotfiles,
     chaotic,
     ... 
   }@ inputs: let
       inherit (self) outputs; 
-
       # Load helpers globally
       helpers = import ./helpers { lib = nixpkgs.lib; };
 
@@ -93,6 +121,57 @@
     in
     {
       inherit helpers;
+
+            ## BEGIN DARWIN TEST --
+      darwinSystems = [ "aarch64-darwin" ];
+      forAllSystems = f: nixpkgs.lib.genAttrs (darwinSystems) f;
+      user = "khernand";
+
+      mkApp = scriptName: system: {
+        type = "app";
+        program = "${(nixpkgs.legacyPackages.${system}.writeScriptBin scriptName ''
+          #!/usr/bin/env bash
+          PATH=${nixpkgs.legacyPackages.${system}.git}/bin:$PATH
+          echo "Running ${scriptName} for ${system}"
+          exec ${self}/apps/${system}/${scriptName}
+        '')}/bin/${scriptName}";
+      };
+
+      mkDarwinApps = system: {
+        "apply" = mkApp "apply" system;
+        "build" = mkApp "build" system;
+        "build-switch" = mkApp "build-switch" system;
+        "copy-keys" = mkApp "copy-keys" system;
+        "create-keys" = mkApp "create-keys" system;
+        "check-keys" = mkApp "check-keys" system;
+        "rollback" = mkApp "rollback" system;
+      };
+
+      darwinConfigurations = nixpkgs.lib.genAttrs darwinSystems (system:
+        darwin.lib.darwinSystem {
+          inherit system;
+          specialArgs = inputs // { inherit user; };
+          modules = [
+            home-manager.darwinModules.home-manager
+            nix-homebrew.darwinModules.nix-homebrew
+            {
+              nix-homebrew = {
+                inherit user;
+                enable = true;
+                taps = {
+                  "homebrew/homebrew-core" = homebrew-core;
+                  "homebrew/homebrew-cask" = homebrew-cask;
+                  "homebrew/homebrew-bundle" = homebrew-bundle;
+                };
+                mutableTaps = false;
+                autoMigrate = true;
+              };
+            }
+            ./hosts/darwin
+          ];
+        }
+      );
+      ## END DARWIN TEST --
 
       nixosConfigurations = {
         nix-desktop =  mkNixosConfiguration "nix-desktop" "khernand";
